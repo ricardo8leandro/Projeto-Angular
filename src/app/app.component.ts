f962279864ff385cbe1c010b2495c8ec65d38f55
Ricardo <ricardo8leandro@gmail.com>
2022-05-06 21:03:26 -0300

    Concept Decoration, Imports - Interpolation and concept one way data binding e two way data binding through de ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-manager';
+  title = 'course-manager-dio';
+
+  name: string = 'Ricardo';
+
 }
